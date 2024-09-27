b9df85e3a7360efd2091157644f86b97fb0fcfe5
Dj-Artimus-X <Pratik.djArtimus@gmail.com>
2024-09-27 05:57:11 +0530

    Added some changes

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,35 @@ export default defineConfig({
             type: "image/png"
           }
         ]
-      }
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\/api\/.*\/*.json/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50, // limit the number of entries
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // 1 week
+              },
+            },
+          },
+        ],
+      },
+      srcDir: 'public',
+      filename: 'sw.js',
     })
   ]
 });
